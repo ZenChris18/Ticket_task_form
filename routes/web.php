5d88e0e53b9959651daccb538ed1d7bd5e3376ac
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2024-07-19 03:32:46 +0800

    added comments

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\FormController;
 Route::get('/', [FormController::class, 'showProfileForm'])->name('show.form');
 Route::post('/submit-form', [FormController::class, 'submitForm'])->name('submit.form');
 Route::get('Attendance', [FormController::class, 'showEmp_DTRForm'])->name('Emp_DTR');
+
+//add other routes for other
